9949982dd56d302adcb7e46f6dfe1ad2595f81a4
shreyansh232 <shreyansh487@gmail.com>
2024-04-20 19:07:55 +0530

    Featured Courses page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import HeroSection from "@/components/ui/HeroSection";
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
-      </main>
+      <FeaturedCourses />
+    </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 text-xl border-[0.1px] border-white rounded-full",
+        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 text-xl rounded-full",
         className
       )}
     >
